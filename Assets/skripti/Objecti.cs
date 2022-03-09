b4f0a2d245f1d4ae95e8c347552b2f5dc5cc4320
kaaaaaaaaaaarl <caalis18@gmail.com>
2022-03-09 11:11:06 +0200

    kautko aizmirsu

--- Assets/skripti/Objecti.cs
+++ Assets/skripti/Objecti.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Object : MonoBehaviour {
+public class Objecti : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- Assets/skripti/dragdrop.cs
+++ Assets/skripti/dragdrop.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class dragdrop : MonoBehaviour {
+public class dragdrop : MonoBehaviour, MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
+	private CanvasGroup kanvasGrupa;
+	private RectTransform transformGupa;
+	public Object jaunsSkripka;
 	// Use this for initialization
 	void Start () {
-		
+		kanvasGrupa = GetComponent<CanvasGroup> ();
+		transformGupa = GetComponent<RectTransform>();
 	}
-	
+	public void onPointerDown(PointerEventData notikums){
+		Debug.Log ("uzspiests");
+	}
+
+	public void onBeginDrag(PointerEventData notikums){
+		Debug.Log ("uzsakta vilksana");
+		kanvasGrupa.alpha = 0.6f;
+		kanvasGrupa.blocksRaycasts = false;
+	}
+	public void onDrag(PointerEventData notikums){
+		Debug.Log ("uzsakta vilksana");
+		transformGupa.anchoredPosition += notikums.delta / objektuSkripts.kanva.scaleFactor;
+
+	}
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
-	}
 }
