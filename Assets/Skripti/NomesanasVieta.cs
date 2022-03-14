549e79ba885fa403d0f84cc30926832177c34350
unknown <caalis18@gmail.com>
2022-03-14 21:52:34 +0200

    majas taisitais

--- Assets/Skripti/NomesanasVieta.cs
+++ Assets/Skripti/NomesanasVieta.cs
@@ -44,6 +44,8 @@ public class NomesanasVieta : MonoBehaviour, IDropHandler {
 					notikums.pointerDrag.GetComponent<RectTransform> ().localScale
 					= GetComponent<RectTransform> ().localScale;
 
+
+
 					//Pārbauda tagu un atskaņo atbilstošo skaņas efektu
 					switch (notikums.pointerDrag.tag) {
 					case "Atkritumi":
@@ -57,6 +59,34 @@ public class NomesanasVieta : MonoBehaviour, IDropHandler {
 					case "Skola":
 						objektuSkripts.skanasAvots.PlayOneShot (objektuSkripts.skanaKoAtskanot [3]);
 						break;
+
+					case "b2":
+						objektuSkripts.skanasAvots.PlayOneShot (objektuSkripts.skanaKoAtskanot [4]);
+						break;
+					case "e46":
+						objektuSkripts.skanasAvots.PlayOneShot (objektuSkripts.skanaKoAtskanot [5]);
+						break;
+					case "cementaMasina":
+						objektuSkripts.skanasAvots.PlayOneShot (objektuSkripts.skanaKoAtskanot [6]);
+						break;
+					case "policija":
+						objektuSkripts.skanasAvots.PlayOneShot (objektuSkripts.skanaKoAtskanot [7]);
+						break;
+					case "traktor1":
+						objektuSkripts.skanasAvots.PlayOneShot (objektuSkripts.skanaKoAtskanot [8]);
+						break;case "b2":
+						objektuSkripts.skanasAvots.PlayOneShot (objektuSkripts.skanaKoAtskanot [9]);
+						break;
+					case "traktor2":
+						objektuSkripts.skanasAvots.PlayOneShot (objektuSkripts.skanaKoAtskanot [10]);
+						break;
+					case "udens":
+						objektuSkripts.skanasAvots.PlayOneShot (objektuSkripts.skanaKoAtskanot [11]);
+						break;
+					case "eskavator":
+						objektuSkripts.skanasAvots.PlayOneShot (objektuSkripts.skanaKoAtskanot [12]);
+						break;
+
 
 					default:
 						Debug.Log ("Nedefinēts tags!");
@@ -87,6 +117,41 @@ public class NomesanasVieta : MonoBehaviour, IDropHandler {
 					= objektuSkripts.bussKoord;
 					break;
 
+				case "b2":
+					objektuSkripts.b2.GetComponent<RectTransform> ().localPosition 
+					= objektuSkripts.bussKoord;
+					break;
+				case "e46":
+					objektuSkripts.e46.GetComponent<RectTransform> ().localPosition 
+					= objektuSkripts.bussKoord;
+					break;
+				case "cementaMasina":
+					objektuSkripts.cementaMasina.GetComponent<RectTransform> ().localPosition 
+					= objektuSkripts.bussKoord;
+					break;
+				case "policija":
+					objektuSkripts.policija.GetComponent<RectTransform> ().localPosition 
+					= objektuSkripts.bussKoord;
+					break;
+				case "traktor1":
+					objektuSkripts.traktor1.GetComponent<RectTransform> ().localPosition 
+					= objektuSkripts.bussKoord;
+					break;
+				case "traktor2":
+					objektuSkripts.traktor2.GetComponent<RectTransform> ().localPosition 
+					= objektuSkripts.bussKoord;
+					break;
+				case "udens":
+					objektuSkripts.udens.GetComponent<RectTransform> ().localPosition 
+					= objektuSkripts.bussKoord;
+					break;
+				case "eskavator":
+					objektuSkripts.eskavator.GetComponent<RectTransform> ().localPosition 
+					= objektuSkripts.bussKoord;
+					break;
+
+
+
 				default:
 					Debug.Log ("Nedefinēts tags!");
 					break;
@@ -96,3 +161,16 @@ public class NomesanasVieta : MonoBehaviour, IDropHandler {
 		}
 	}
 }
+
+/*
+	public GameObject atkritumuMasina;
+	public GameObject atraPalidziba;
+	public GameObject autobuss;
+	public GameObject b2;
+	public GameObject e46;
+	public GameObject cementaMasina;
+	public GameObject policija;
+	public GameObject traktor1;
+	public GameObject traktor2;
+	public GameObject udens;
+	public GameObject eskavator;*/
--- Assets/Skripti/Objekti.cs
+++ Assets/Skripti/Objekti.cs
@@ -9,6 +9,14 @@ public class Objekti : MonoBehaviour {
 	public GameObject atkritumuMasina;
 	public GameObject atraPalidziba;
 	public GameObject autobuss;
+	public GameObject b2;
+	public GameObject e46;
+	public GameObject cementaMasina;
+	public GameObject policija;
+	public GameObject traktor1;
+	public GameObject traktor2;
+	public GameObject udens;
+	public GameObject eskavator;
 
 	//Uzglabā velakmo objektu sākotnējās atrašanās vietas koordinātas
 	[HideInInspector]
@@ -17,6 +25,22 @@ public class Objekti : MonoBehaviour {
 	public Vector2 atroKoord;
 	[HideInInspector]
 	public Vector2 bussKoord;
+	[HideInInspector]
+	public Vector2 b2koord;
+	[HideInInspector]
+	public Vector2 cementakoord;
+	[HideInInspector]
+	public Vector2 e46koord;
+	[HideInInspector]
+	public Vector2 eskavatorskoord;
+	[HideInInspector]
+	public Vector2 traktor1koord;
+	[HideInInspector]
+	public Vector2 policijakoord;
+	[HideInInspector]
+	public Vector2 traktor2koord;
+	[HideInInspector]
+	public Vector2 Udenskoord;
 
 	//Uzglabās audio avotu, kurā atskaņot attēlu skaņas efektus
 	public AudioSource skanasAvots;
@@ -33,5 +57,13 @@ public class Objekti : MonoBehaviour {
 		atkrKoord = atkritumuMasina.GetComponent<RectTransform> ().localPosition;
 		atroKoord = atraPalidziba.GetComponent<RectTransform> ().localPosition;
 		bussKoord = autobuss.GetComponent<RectTransform> ().localPosition;
+		b2koord = b2.GetComponent<RectTransform> ().localPosition;
+		cementakoord = cementaMasina.GetComponent<RectTransform> ().localPosition;
+		e46koord = e46.GetComponent<RectTransform> ().localPosition;
+		eskavatorskoord = eskavator.GetComponent<RectTransform> ().localPosition;
+		traktor1koord = traktor1.GetComponent<RectTransform> ().localPosition;
+		policijakoord = policija.GetComponent<RectTransform> ().localPosition;
+		traktor2koord = traktor2.GetComponent<RectTransform> ().localPosition;
+		Udenskoord = udens.GetComponent<RectTransform> ().localPosition;
 	}
 }
